cb1800440330187fd4b7059500fb60cf13d4223a
Mahi <agrawalmahi18012006@gmail.com>
2026-04-24 15:22:18 +0530

    leetcode problems

---
+++ Leetcode169.java
@@ -0,0 +1,30 @@
+ class LeetCode169 {
+    public int majorityElement(int[] nums) {
+        return sol(nums);
+        
+    }
+    public static int sol(int arr[])
+    {
+        int cnt=0;
+        int val=-1;
+        int i=0;
+        while(i<arr.length)
+        {
+            if(cnt==0)
+            {
+                cnt=1;
+                val=arr[i];
+            }
+            else if(arr[i]==val)
+            {
+                cnt+=1;
+            }
+            else
+            {
+                cnt--;
+            }
+            i++;
+        }
+        return val;
+    }
+}
