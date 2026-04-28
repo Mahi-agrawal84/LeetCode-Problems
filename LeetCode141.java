f32a82171006e7da264bd392bfaf6622f1e193cb
Mahi <agrawalmahi18012006@gmail.com>
2026-04-28 20:39:14 +0530

    push LeetCode Problems

--- LeetCode141.java
+++ LeetCode141.java
@@ -1,5 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class LeetCode141 {
     public boolean hasCycle(ListNode head) {
+        return sol(head);
         
+    }
+    public boolean sol(ListNode head)
+    {
+        ListNode slow=head;
+        ListNode fast=head;
+        boolean flag=true;
+        while(fast!=null && fast.next!=null)
+        {
+            if(slow==fast)
+            {
+                if(slow==fast && flag==false)
+                {
+                    return true;
+
+                }
+                flag= false;
+            }
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return false ;
     }
 }
--- LeetCode142.java
+++ LeetCode142.java
@@ -1,5 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class LeetCode142 {
     public ListNode detectCycle(ListNode head) {
+        return sol(head);
         
+    }
+    public ListNode sol(ListNode head)
+    {
+        if(head==null)return null;
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null )
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast)
+            {
+                int cnt=getcnt(fast);
+                ListNode temp=head;
+                int idx=0;
+                while(cnt>idx)
+                {
+                    temp=temp.next;
+                    idx++;
+                }
+                while(temp!=head)
+                {
+                    temp=temp.next;
+                    head=head.next;
+                }
+                return temp;
+            }
+        }
+        return null;
+    }
+    public int getcnt(ListNode fast)
+    {
+        ListNode temp=fast.next;
+        int cnt=1;
+        while(temp!=fast)
+        {
+            temp=temp.next;
+            cnt++;
+        }
+        return cnt;
     }
 }
